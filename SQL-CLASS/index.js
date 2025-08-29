4dc961d538a69feb99c903188c75af978acf2a5b
Rajaabpro <rajaab.pro@gmail.com>
2025-08-29 15:23:25 +0500

    feat: enhance index.js to insert 100 fake users into the database and set up an Express server

--- SQL-CLASS/index.js
+++ SQL-CLASS/index.js
@@ -1,28 +1,49 @@
-const { faker } = require("@faker-js/faker");
-const mysql = require("mysql2/promise");
+const faker = require("@faker-js/faker");
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
 
-let createConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Deepchatb@1",
-    database: "delta_app",
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "delta_app",
+  password: "mysql@123",
 });
-try{    
-    let result = await createConnection.query("SHOW TABLES");
-    console.log(result);
-    createConnection.end();
-}catch(err){
-    console.log(err);
-}
-
 
 let getRandomUser = () => {
-    return {
-        id: faker.string.uuid(),
-        username: faker.internet.username(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    }
+  return [
+    faker.datatype.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+//Inserting New Data
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+let data = [];
+for (let i = 1; i <= 100; i++) {
+  data.push(getRandomUser()); // 100 fake users
 }
 
-console.log(getRandomUser());
+try {
+  connection.query(q, [data], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
+
+
+app.get("/", (req, res) => {
+    res.send(" Welcome to the Delta App");
+  });
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
