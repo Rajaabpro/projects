32ab06a056298a89ae2ad56c9e86f5251b17f33c
Rajaabpro <rajaab.pro@gmail.com>
2025-09-02 22:08:00 +0500

    feat: enhance index.js to establish MongoDB connection and serve a styled Hello World response

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+app.get('/', (req, res) => {
+  res.send('<style>body { background-color:rgb(16, 15, 15); color:white; }</style>Hello World');
+}); 
+
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
