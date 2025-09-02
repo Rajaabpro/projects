d55398372e907636fcbd1b577581d4dd73d6841a
Rajaabpro <rajaab.pro@gmail.com>
2025-09-02 15:34:31 +0500

    feat: add book instance creation and saving logic in books.js for MongoDB

--- MONGO-DB/books.js
+++ MONGO-DB/books.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 
 main().then(() => {
-  console.log('Connected to MongoDB successfully !! 🎉 ');
+  console.log('Connected to MongoDB successfully !! 🎉 Amazon DB ');
 })
 .catch(err => {
   console.log(err);
-  console.log('Failed to connect to MongoDB !! 🔴 ');
+  console.log('Failed to connect to MongoDB !! 🔴 Amazon DB ');
 });
 
 async function main() {
@@ -27,4 +27,18 @@ const bookSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+let book1 = new Book({
+    title: 'How to win friends and influence people',
+    author: 'Dale Carnegie',
+    price: 250,
+});
+
+book1.save().then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
 });
