5d586a43509ade9a0133955df3fc12f0d1da97fe
Rajaabpro <rajaab.pro@gmail.com>
2025-09-03 14:40:49 +0500

    refactor: update chat model reference and enhance routing in index.js for chat retrieval and home page

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const path = require('path');
-const chat = require('./models/chat');
+const Chat = require('./models/chat');
 
 // EJS setup
 app.set("views", path.join(__dirname, "views"));
@@ -18,22 +18,26 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
-let chat1 = new chat({
-  from: 'John',
+//Index Route
+app.get('/chats', async (req, res) => {
+  let chats = await Chat.find({});
+  console.log(chats);
+  res.render('index.ejs', { chats });
+});
+
+//Home Route
+app.get('/', (req, res) => {
+  res.send('<style>body { background-color:rgb(16, 15, 15); color:white; }</style> Rot is running');
+}); 
+
+let chat1 = new Chat({
+  from: 'John1',
   to: 'Jane',
   msg: 'Hello how are you?',
   created_at: new Date(),
 });
 
-chat1.save().then((res) => {
-  console.log(res);
-}).catch((err) => {
-  console.log(err);
-});
 
-app.get('/', (req, res) => {
-  res.send('<style>body { background-color:rgb(16, 15, 15); color:white; }</style>Hello World');
-}); 
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} 🎉 `);
