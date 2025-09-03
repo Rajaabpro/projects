0d5aac0c1a15f47c9ef86ee1b7d90f307ebc832e
Rajaabpro <rajaab.pro@gmail.com>
2025-09-03 14:40:42 +0500

    feat: add sample chat data to init.js for initial database population

--- MONGO3/init.js
+++ MONGO3/init.js
@@ -13,6 +13,39 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+let allChats = [
+    {
+        from: 'John',
+        to: 'Jane',
+        msg: 'Hello how are you?',
+        created_at: new Date(),
+    },
+    {
+        from: 'Jane',
+        to: 'John',
+        msg: 'I am fine, thank you?',
+        created_at: new Date(),
+    },
+    {
+        from: 'John',
+        to: 'Jane',
+        msg: 'What are you doing?',
+        created_at: new Date(),
+    },
+    {
+        from: 'Jane',
+        to: 'John',
+        msg: 'I am doing nothing, you?',
+        created_at: new Date(),
+    },
+    {
+        from: 'John',
+        to: 'Jane',
+        msg: 'I am doing nothing, you?',
+        created_at: new Date(),
+    },
+];
+
 
 chat.insertMany([
     {
@@ -22,3 +55,5 @@ chat.insertMany([
         created_at: new Date(),
     },
 ]);
+
+chat.insertMany(allChats);
