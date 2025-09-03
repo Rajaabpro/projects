52c972b0fd24372249c0b75f5ae510d8aa447a1e
Rajaabpro <rajaab.pro@gmail.com>
2025-09-03 14:00:39 +0500

    feat: integrate chat model into index.js and implement initial chat message saving functionality

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const path = require('path');
+const chat = require('./models/chat');
+
+// EJS setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Database connection
 main()
 .then(() => console.log('Connected to MongoDB 🎉 '))
 .catch(err => console.log(err, 'Failed to connect to MongoDB 🔴 '));
@@ -12,6 +17,19 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+
+let chat1 = new chat({
+  from: 'John',
+  to: 'Jane',
+  msg: 'Hello how are you?',
+  created_at: new Date(),
+});
+
+chat1.save().then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.log(err);
+});
 
 app.get('/', (req, res) => {
   res.send('<style>body { background-color:rgb(16, 15, 15); color:white; }</style>Hello World');
